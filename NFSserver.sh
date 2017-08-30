3b71a5facbe76416382affe5049dc94e59af5496
Joel-hou <yanhuaqingshang@163.com>
2017-08-30 11:05:32 +0800

    add nfs server configuration script

--- NFSserver.sh
+++ NFSserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # on one controller node
-# configure NFS Server
+# bash script to configure NFS Server, execute on controller nodes
 
 mkdir -p /var/lib/nova/instances/
 
@@ -17,11 +17,39 @@ sed -i "s|^#Nobody-User.*$|Nobody-User = nobody|" \
 sed -i "s|^#Nobody-Group.*$|Nobody-Group = nobody|" \
 /etc/idmapd.conf
 
+# /etc/libvirt/libvirtd.conf
 sed -i "s|^#listen_tls =.*$|listen_tls = 0|" \
 /etc/libvirt/libvirtd.conf
 sed -i "s|^#listen_tcp =.*$|listen_tcp = 1|" \
 /etc/libvirt/libvirtd.conf
-
 sed -i 's|^#auth_tcp = .*$|auth_tcp = "none"|' \
 /etc/libvirt/libvirtd.conf
+sed -i 's|^#unix_sock_group = .*$|unix_sock_group= "libvirtd"|' \
+/etc/libvirt/libvirtd.conf
+sed -i 's|^#unix_sock_ro_perms = .*$|unix_sock_ro_perms= "0777"|' \
+/etc/libvirt/libvirtd.conf
+sed -i 's|^#unix_sock_rw_perms = .*$|unix_sock_rw_perms= "0770"|' \
+/etc/libvirt/libvirtd.conf
+sed -i 's|^#auth_unix_ro = .*$|auth_unix_ro= "none"|' \
+/etc/libvirt/libvirtd.conf
+sed -i 's|^#auth_unix_rw = .*$|auth_unix_rw= "none"|' \
+/etc/libvirt/libvirtd.conf
+
+# configure qemu
+# /etc/libvirt/qemu.conf
+sed -i 's|^#vnc_listen = .*$|vnc_listen= "0.0.0.0"|' \
+/etc/libvirt/qemu.conf
+sed -i 's|^#user = .*$|user= "root"|' \
+/etc/libvirt/qemu.conf
+sed -i 's|^#group = .*$|group= "root"|' \
+/etc/libvirt/qemu.conf
+sed -i 's|^#dynamic_ownership = .*$|dynamic_ownership= "0"|' \
+/etc/libvirt/qemu.conf
+
+# restart service to make changes take effect
+systemctl restart rpcidmapd.service
+systemctl restart nfs-server.service
+
+
+
 
