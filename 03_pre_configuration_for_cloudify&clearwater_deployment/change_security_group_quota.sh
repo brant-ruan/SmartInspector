7ecb79463f844f9cb51cffe87228843505d74a52
Joel-hou <yanhuaqingshang@163.com>
2017-09-12 09:15:05 +0800

    add ansible

--- 03_pre_configuration_for_cloudify&clearwater_deployment/change_security_group_quota.sh
+++ 03_pre_configuration_for_cloudify&clearwater_deployment/change_security_group_quota.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# this should be executed in all of the controller nodes
+# use ansible for your configuration
+# ansible controller -m script -a "./change_security_group_quota.sh" --sudo
+
 neutronConf="/etc/neutron/neutron.conf"
 sed -i "s|^#quota_security_group.*$|quota_security_group = 50|" \
 $neutronConf
