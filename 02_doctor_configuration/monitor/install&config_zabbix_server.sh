0689c9b77c8dd8700348abb2b08c933bfc307ac2
Joel-hou <yanhuaqingshang@163.com>
2017-09-14 14:43:10 +0800

    fixed mysql syntax error

--- 02_doctor_configuration/monitor/install&config_zabbix_server.sh
+++ 02_doctor_configuration/monitor/install&config_zabbix_server.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# sudo needed
 # this script is used for installing and config zabbix-server on openstack controller node.
-
 # firstly make sure mysql is started
 sudo systemctl start mariadb 
 sudo systemctl enable mariadb
 sudo rpm -ivh http://repo.zabbix.com/zabbix/3.0/rhel/7/x86_64/zabbix-release-3.0-1.el7.noarch.rpm
 sudo yum install zabbix-server-mysql zabbix-web-mysql -y
-# you could choose to monitor zabbix server node itself
+# you could choose to monitor zabbix server node itself via uncomment the following command
 # yum install zabbix-agent
 
-mysql -u root -e "create database zabbix character set utf8;
+# create datebase for zabbix
+sudo mysql -u root -e "create database zabbix character set utf8;
 grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';
 flush privileges;
-quit;"
-
-sudo cd /usr/share/doc/zabbix-server-mysql-3.0.10/
-zcat create.sql.gz | mysql -uzabbix -p zabbix
+"
+cd /usr/share/doc/zabbix-server-mysql-3.0.10/
+zcat create.sql.gz | mysql -uzabbix -pzabbix
+# if any error occurs,zcat create.sql.gz | mysql -uzabbix -p then type your zabbix password "zabbix" when promoted
 sudo sed -i "s/# DBPassword=/DBPassword=zabbix/g" /etc/zabbix/zabbix_server.conf
 sudo sed -i "s/# php_value date.timezone Europe\/Riga/php_value date.timezone Asia\/Shanghai/g" /etc/httpd/conf.d/zabbix.conf
 
