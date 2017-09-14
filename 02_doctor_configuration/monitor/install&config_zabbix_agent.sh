ebec19a9291374ec51c4dd515b95b3ba9d203846
Joel-hou <yanhuaqingshang@163.com>
2017-09-14 10:06:52 +0800

    remove empty lines & zabbix monitor NIC status

--- 02_doctor_configuration/monitor/install&config_zabbix_agent.sh
+++ 02_doctor_configuration/monitor/install&config_zabbix_agent.sh
@@ -6,27 +6,19 @@
 # $1 zabbix-server ip 
 # $2 PSK sequence number  such as : 001、002、003
 
-rpm -ivh http://repo.zabbix.com/zabbix/3.0/rhel/7/x86_64/zabbix-release-3.0-1.el7.noarch.rpm
+sudo rpm -ivh http://repo.zabbix.com/zabbix/3.0/rhel/7/x86_64/zabbix-release-3.0-1.el7.noarch.rpm
+sudo yum install zabbix-agent
 
-yum install zabbix-agent
+sudo sh -c "openssl rand -hex 32 > /etc/zabbix/zabbix_agentd.psk"
+sudo sed -i "s/Server=127.0.0.1/Server=$1/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/# TLSConnect=unencrypted/TLSConnect=psk/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/# TLSAccept=unencrypted/TLSAccept=psk/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/# TLSPSKIdentity=/TLSPSKIdentity=PSK $2/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/# TLSPSKFile=/TLSPSKFile=\/etc\/zabbix\/zabbix_agentd.psk/g" /etc/zabbix/zabbix_agentd.conf
 
-sh -c "openssl rand -hex 32 > /etc/zabbix/zabbix_agentd.psk"
-
-sed -i "s/Server=127.0.0.1/Server=$1/g" /etc/zabbix/zabbix_agentd.conf
-
-sed -i "s/# TLSConnect=unencrypted/TLSConnect=psk/g" /etc/zabbix/zabbix_agentd.conf
-
-sed -i "s/# TLSAccept=unencrypted/TLSAccept=psk/g" /etc/zabbix/zabbix_agentd.conf
-
-sed -i "s/# TLSPSKIdentity=/TLSPSKIdentity=PSK $2/g" /etc/zabbix/zabbix_agentd.conf
-
-sed -i "s/# TLSPSKFile=/TLSPSKFile=\/etc\/zabbix\/zabbix_agentd.psk/g" /etc/zabbix/zabbix_agentd.conf
-
-systemctl start zabbix-agent 
-
-systemctl enable zabbix-agent
-
-iptables -I INPUT -p tcp --dport 10050 -j ACCEPT
+sudo systemctl start zabbix-agent 
+sudo systemctl enable zabbix-agent
+sudo iptables -I INPUT -p tcp --dport 10050 -j ACCEPT
 
 
 
---
+++ 02_doctor_configuration/monitor/zabbix_monitor_NIC_status.md
@@ -0,0 +1,1 @@
+# Zabbix monitor NIC status
