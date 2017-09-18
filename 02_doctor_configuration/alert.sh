06c47214f0679b25e73ae6202aab7cc957378be1
Joel-hou <yanhuaqingshang@163.com>
2017-09-18 16:08:14 +0800

    nothing

--- 02_doctor_configuration/alert.sh
+++ 02_doctor_configuration/alert.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
-#author: zang yuan
-#send notification to congress
 
-username="admin"
-password="4MbCBXaqHJmARqpWzFNJFQF2T"
-congress_url="http://192.168.37.12:1789"
-keystone_url="http://192.168.37.12:5000/v2.0"
+USER_NAME="admin"
+PASSWORD="FZKueDaeuZ72nuDHXjmzfBkzD"
+CONGRESS_URL="http://192.0.2.9:1789"
+KEYSTONE_URL="http://192.0.2.9:35357/v2.0"
+COMPUTE_HOSTNAME = "overcloud-novacompute-0.opnfvlf.org"
 
-curl $keystone_url"/tokens" -X POST -H "Content-Type: application/json" -H "Accept: application/json"  -d "{\"auth\": {\"tenantName\": $username, \"passwordCredentials\": {\"username\": $username, \"password\": $password}}}" > result.json
+curl $KEYSTONE_URL"/tokens" -X POST -H "Content-Type: application/json" -H "Accept: application/json"  -d "{\"auth\": {\"tenantName\": \"admin\", \"passwordCredentials\": {\"username\": \"admin\", \"password\": \"$PASSWORD\"}}}" > result.json
 token0=$(jq .access.token.id result.json)
 echo $token0
 token=${token0//\"/}
 echo $token
-
-rm result.json
-#overcloud-novacompute-0.opnfvlf.org
+rm -f result.json
 
 #sendtime=`date`
 eventtype="compute.host.down"
-hostname="overcloud-novacompute-0.opnfvlf.org"
 date=$(date +%Y-%m-%d)
 hms=$(date +%H:%M:%S)
-sendtime=${date}T${hms}Z 
-curl -i -X PUT $congress_url/v1/data-sources/doctor/tables/events/rows -H "Content-Type: application/json" -d "[{\"time\":$sendtime,\"type\":$eventtype,\"details\":{\"hostname\":$hostname,\"status\":\"down\",\"monitor\":\"zabbix1\",\"monitor_event_id\":\"111\"}}]"  -H "X-Auth-Token: $token"
+SENDTIME=${date}T${hms}Z
 
-
-#Linux date format Thu Aug 31 03:06:58 UTC 2017
-
-#congress accept time format "2016-02-22T11:48:55Z"
+curl -i -X PUT $CONGRESS_URL/v1/data-sources/doctor/tables/events/rows -H "Content-Type: application/json" -d "[{\"time\":\"$SENDTIME\",\"type\":\"compute.host.down\",\"details\":{\"hostname\":\"$COMPUTE_HOSTNAME\",\"status\":\"down\",\"monitor\":\"zabbix1\",\"monitor_event_id\":\"111\"}}]" -H "X-Auth-Token: $token"
+#curl -i -X PUT $CONGRESS_URL/v1/data-sources/doctor/tables/events/rows -H "Content-Type: application/json" -d '[{"time":"2016-02-22T11:48:55Z","type":"compute.host.down","details":{"hostname":"overcloud-novacompute-0.opnfvlf.org","status":"down","monitor":"zabbix1","monitor_event_id":"111"}}]'  -H "X-Auth-Token: $token"
+#curl -i -X PUT $congress_url/v1/data-sources/doctor/tables/events/rows -H "Content-Type: application/json" -d '[{\"time\":"2016-02-22T11:48:55Z",\"type\":\"compute.host.down\","details":{"hostname":"overcloud-novacompute-0.opnfvlf.org","status":"down","monitor":"zabbix1","monitor_event_id":"111"}}]'  -H "X-Auth-Token: $token"
